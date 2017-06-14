390fe45159ff5ba019ee8893d6e326b1ace63310
Benjamin Charity <ben@benjamincharity.com>
2017-06-14 09:50:13 -0400

    style: whitespace cleanup

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TerminusUIModule } from '@terminus/ui';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, TerminusUIModule],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    TerminusUIModule,
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
 })
 export class AppModule { }
