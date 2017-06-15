5425c6582949b30a981ec59ee28361caf0353560
Benjamin Charity <ben@benjamincharity.com>
2017-06-15 11:26:22 -0400

    chore(Demo): add components to the demo

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TerminusUIModule } from '@terminus/ui';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TsButtonModule,
+  TsInputModule,
+  TsSearchModule,
+} from '@terminus/ui';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    TerminusUIModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    TsButtonModule,
+    TsInputModule,
+    TsSearchModule,
   ],
   declarations: [
     AppComponent,
