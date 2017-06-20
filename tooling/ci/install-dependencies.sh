ddb5d885b7ddc7980dc3c0402b3d5293880d70db
Benjamin Charity <ben@benjamincharity.com>
2017-06-20 17:18:15 -0400

    chore(CI): clean up dependency install

--- tooling/ci/install-dependencies.sh
+++ tooling/ci/install-dependencies.sh
@@ -7,19 +7,11 @@
 # 2) Install all project dependencies
 #
 
-echo 'VERSIONS: '
-echo 'NODE: '
-node --version
-echo 'NPM: '
-npm --v
-echo 'YARN: '
-yarn --version
-echo '_______________'
 
 # Add all needed paths
 # FIXME: This should not be needed since it is being done in the docker image. Removing it causes an
 # 'aws not found' error
-echo 'export PATH=/usr/local/bin:~/.yarn/bin:~/.local/bin:$PATH' >>~/.bashrc
+#echo 'export PATH=/usr/local/bin:~/.yarn/bin:~/.local/bin:$PATH' >>~/.bashrc
 
 # NOTE: Currently `yarn install` does not correctly install all executables under
 # `node_modules/.bin/`. NPM does seem to correctly do this. Once yarn is correctly installing all
