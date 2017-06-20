071ecb93e81359609917343815673f41b5436e6a
Benjamin Charity <ben@benjamincharity.com>
2017-06-20 08:04:31 -0400

    WIP: no longer manually updating npm

--- tooling/ci/install-dependencies.sh
+++ tooling/ci/install-dependencies.sh
@@ -12,19 +12,17 @@
 # 'aws not found' error
 echo 'export PATH=/usr/local/bin:~/.yarn/bin:~/.local/bin:$PATH' >>~/.bashrc
 
+npm --v
+
 # NOTE: Currently `yarn install` does not correctly install all executables under
 # `node_modules/.bin/`. NPM does seem to correctly do this. Once yarn is correctly installing all
 # `.bin` items, this should be switched back to `yarn install`
-npm i npm@latest -g
-
-npm --v
-
 npm i
 
-echo 'INSTALLING TEST REPORTER';
+#echo 'INSTALLING TEST REPORTER';
 
-# TODO: If this works, move the install to our docker image
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+## TODO: If this works, move the install to our docker image
+#curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 
-chmod +x ./cc-test-reporter
+#chmod +x ./cc-test-reporter
 
