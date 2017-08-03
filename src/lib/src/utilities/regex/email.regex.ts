6099c1e166b564f2fe5a0470c5c6a6b89024654a
Benjamin Charity <ben@benjamincharity.com>
2017-08-03 08:47:38 -0400

    feat(Regex): create regex's for common items
    These will be used for form validation

---
+++ src/lib/src/utilities/regex/email.regex.ts
@@ -0,0 +1,6 @@
+/* tslint:disable:max-line-length */
+
+/**
+ * Define a regex to validate an email address
+ */
+export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
---
+++ src/lib/src/utilities/regex/password.regex.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:max-line-length */
+
+/**
+ * Define a regex to validate an email address
+ *
+ * {6,100}     - Assert password is between 6 and 100 characters
+ * (?=.*[0-9]) - Assert a string has at least one number
+ */
+export const passwordRegex = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/;
