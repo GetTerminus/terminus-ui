25db724f0997b74b4e0148db2757912830a35f54
Benjamin Charity <ben@benjamincharity.com>
2017-08-07 19:06:41 -0400

    WIP: add more debug statements

--- tooling/ci/publish.sh
+++ tooling/ci/publish.sh
@@ -14,6 +14,17 @@ echo "Tag: ${TAG}"
 echo "Branch: ${CIRCLE_BRANCH}"
 echo "Token: ${NPM_TOKEN}"
 
+echo 'Tag: ' $CIRCLE_TAG
+echo 'Build Number: ' $CIRCLE_BUILD_NUM
+
+echo 'Job Type' $CIRCLE_JOB
+echo 'Compare URL' $CIRCLE_COMPARE_URL
+echo 'Pull Request' $CIRCLE_PULL_REQUEST
+echo 'Build Number' $CIRCLE_BUILD_NUM
+echo 'Build URL' $CIRCLE_BUILD_URL
+echo 'Branch' $CIRCLE_BRANCH
+echo 'Node total' $CIRCLECI_NODE_TOTAL
+
 # Run tests
 npm run semantic-release || {
     echo 'No release needed.';
