6652c1827b36490030a5b3cbf91f6880abe0c7bc
Benjamin Charity <ben@benjamincharity.com>
2017-08-08 17:52:08 -0400

    refactor(Type): move tooltip types to separate file

---
+++ src/lib/src/tooltip/tooltip-position.type.ts
@@ -0,0 +1,10 @@
+/**
+ * Define the accepted string values for the {@link TsTooltipComponent} position
+ */
+export type TsTooltipPositionTypes =
+  'left' |
+  'right' |
+  'above' |
+  'below' |
+  'before' |
+  'after';
