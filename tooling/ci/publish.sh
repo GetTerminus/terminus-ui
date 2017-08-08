198ec8f8bfcea2d5118f30680956f4f8a53d2184
Benjamin Charity <ben@benjamincharity.com>
2017-08-08 09:55:49 -0400

    fix(CI): check both package version numbers

--- tooling/ci/publish.sh
+++ tooling/ci/publish.sh
@@ -15,28 +15,39 @@
 echo "RUN: semantic-release pre"
 npm run semantic-release-pre
 
-echo "CD: into dist"
-cd dist/
+echo "BUILD"
+npm run build
 
-echo "RUN: pack"
-npm pack
-
-echo "List files"
-ls -a
-
-echo "RUN: get version"
+echo "RUN: get PRIMARY package version"
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
-echo "VERSION: ${PACKAGE_VERSION}"
+echo "PRIMARY VERSION: ${PACKAGE_VERSION}"
 
-echo "CD: out of dist"
-cd ../
+#echo "CD: into dist"
+#cd dist/
+
+#echo "RUN: pack"
+#npm pack
+
+#echo "List files"
+#ls -a
+
+echo "RUN: get DIST package version"
+PACKAGE_VERSION=$(cat dist/package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+echo "DIST VERSION: ${PACKAGE_VERSION}"
+
+#echo "CD: out of dist"
+#cd ../
 
 echo "RUN: npm publish"
-npm publish
+npm publish dist/
 
 echo "RUN: semantic-release post"
 npm run semantic-release-post
