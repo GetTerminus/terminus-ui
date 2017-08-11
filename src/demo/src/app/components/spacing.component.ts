482657a5cf2c301e65e198059d3b37f58bbf5105
Benjamin Charity <ben@benjamincharity.com>
2017-08-11 12:38:16 -0400

    fix(SCSS): expose spacing map and method, create spacing demo, make spacing directive demo more comp

--- src/demo/src/app/components/spacing.component.ts
+++ src/demo/src/app/components/spacing.component.ts
@@ -5,19 +5,56 @@ import { Component } from '@angular/core';
   styles: [`
     div {
       outline: 1px solid lightblue;
+      padding: 8px;
     }
   `],
   template: `
     <div tsVerticalSpacing="small--3x">
-      Thing 1
+      tsVerticalSpacing="small--3x"
+    </div>
+
+    <div tsVerticalSpacing="small--2x">
+      tsVerticalSpacing="small--2x"
+    </div>
+
+    <div tsVerticalSpacing="small--1x">
+      tsVerticalSpacing="small--1x"
     </div>
 
     <div tsVerticalSpacing>
-      Thing 2
+      tsVerticalSpacing
+    </div>
+
+    <div tsVerticalSpacing="large--1x">
+      tsVerticalSpacing="large--1x"
+    </div>
+
+    <div tsVerticalSpacing="large--2x">
+      tsVerticalSpacing="large--2x"
     </div>
 
     <div tsVerticalSpacing="large--3x">
-      Thing 3
+      tsVerticalSpacing="large--3x"
+    </div>
+
+    <div tsVerticalSpacing="large--4x">
+      tsVerticalSpacing="large--4x"
+    </div>
+
+    <div tsVerticalSpacing="large--5x">
+      tsVerticalSpacing="large--5x"
+    </div>
+
+    <div tsVerticalSpacing="large--6x">
+      tsVerticalSpacing="large--6x"
+    </div>
+
+    <div tsVerticalSpacing="large--7x">
+      tsVerticalSpacing="large--7x"
+    </div>
+
+    <div>
+      Standard content...
     </div>
   `,
 })
--- src/lib/src/utilities/types/button-function.types.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Define the allowed {@link TsButtonComponent} action types
- */
-export type TsButtonFunctionTypes =
-  'button'
-  | 'search'
-  | 'submit';
