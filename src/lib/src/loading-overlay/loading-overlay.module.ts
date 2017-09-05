0fec5b8c4a51cd52c00a5dc6240608b1fc790089
Benjamin Charity <ben@benjamincharity.com>
2017-09-05 14:32:50 -0400

    fix(LoadingOverlay): include window service to loading overlay module

--- src/lib/src/loading-overlay/loading-overlay.module.ts
+++ src/lib/src/loading-overlay/loading-overlay.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TsWindowService } from './../services/window/window.service';
 import { TsLoadingOverlayComponent } from './loading-overlay.component';
 import { TsLoadingOverlayDirective } from './loading-overlay.directive';
 export { TsLoadingOverlayDirective } from './loading-overlay.directive';
@@ -9,6 +10,9 @@ export { TsLoadingOverlayDirective } from './loading-overlay.directive';
 @NgModule({
   imports: [
     CommonModule,
+  ],
+  providers: [
+    TsWindowService,
   ],
   declarations: [
     TsLoadingOverlayDirective,
