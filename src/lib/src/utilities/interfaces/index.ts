c450b55f566026f3e26481bea364cdc2a83d3ef1
Benjamin Charity <ben@benjamincharity.com>
2017-09-06 14:51:48 -0400

    chore: create navigation payload interface

--- src/lib/src/utilities/interfaces/index.ts
+++ src/lib/src/utilities/interfaces/index.ts
@@ -1,3 +1,4 @@
 export { TsLoginFormResponse } from './login-form-response.interface';
 export { TsMenuItem } from './menu-item.interface';
 export * from './navigation-item.interface';
+export * from './navigation-payload.interface';
