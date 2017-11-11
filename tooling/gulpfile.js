8644ca520c718f32068830811940e48346753d08
Benjamin Charity <ben@benjamincharity.com>
2017-11-10 21:38:28 -0500

    chore(ci): clarity refactor, comments, replaced random methods with gulp tasks

--- tooling/gulpfile.js
+++ tooling/gulpfile.js
@@ -1,63 +1,36 @@
 'use strict';
-const fs                 = require('fs');
-const path               = require('path');
-const rollup             = require('rollup');
-const gulp               = require('gulp');
-const glob               = require('glob');
-const ngc                = require('@angular/compiler-cli/src/main').main;
-const execa              = require('execa');
-const inlineResources    = require('./inline-resources');
-const camelCase          = require('camelcase');
-const rollupUglify       = require('rollup-plugin-uglify');
-const mergeFiles         = require('merge-files');
-const nodeResolve        = require('rollup-plugin-node-resolve');
-const commonjs           = require('rollup-plugin-commonjs');
-const replace            = require('replace-in-file');
-const del                = require('del');
-const gulpSourcemaps     = require('gulp-sourcemaps');
-const sass               = require('gulp-sass');
-const sassModuleImporter = require('sass-module-importer');
-const postcss            = require('gulp-postcss');
-const autoprefixer       = require('autoprefixer');
-const nodeSassImport     = require('node-sass-import');
 
+const fs                   = require('fs');
+const path                 = require('path');
+const rollup               = require('rollup');
+const gulp                 = require('gulp');
+const glob                 = require('glob');
+const ngc                  = require('@angular/compiler-cli/src/main').main;
+const execa                = require('execa');
+const inlineResources      = require('./inline-resources');
+const camelCase            = require('camelcase');
+const rollupUglify         = require('rollup-plugin-uglify');
+const rollupNodeResolve    = require('rollup-plugin-node-resolve');
+const rollupCommonjs       = require('rollup-plugin-commonjs');
+const replace              = require('replace-in-file');
+const del                  = require('del');
+const gulpSourcemaps       = require('gulp-sourcemaps');
+const gulpSass             = require('gulp-sass');
+const sassModuleImporter   = require('sass-module-importer');
+const postcss              = require('gulp-postcss');
+const autoprefixer         = require('autoprefixer');
+const nodeSassImport       = require('node-sass-import');
+const gulpStripComments    = require('gulp-strip-json-comments');
+const gulpRemoveEmptyLines = require('gulp-remove-empty-lines');
+const gulpConcat           = require('gulp-concat');
+const gulpReplace          = require('gulp-replace');
+const cloneDeep            = require('lodash.clonedeep');
 
-const libNameWithScope = require('./../package.json').name;
-const libName          = libNameWithScope.slice(libNameWithScope.indexOf('/') + 1);
-const rootFolder       = path.join(__dirname, '../');
+const libNameWithScope     = require('./../package.json').name;
+const libName              = libNameWithScope.slice(libNameWithScope.indexOf('/') + 1);
+const rootFolder           = path.join(__dirname, '../');
 
-const config = {
-  libName: libName,
-  paths: {
-    rootFolder: rootFolder,
-    distFolder: path.join(rootFolder, 'dist'),
-    srcFolder: path.join(rootFolder, 'src/lib'),
-    tempLibFolder: path.join(rootFolder, 'out-tsc', 'lib'),
-    es2015OutputFolder: path.join(rootFolder, 'out-tsc', 'lib-es2015'),
-    es5Entry: path.join(rootFolder, 'out-tsc', 'lib-es5', `${libName}.js`),
-    libFinalOutputFile: path.join(rootFolder, 'dist', `bundles`, `${libName}.umd.js`),
-    libFinalOutputMinifiedFile: path.join(rootFolder, 'dist', `bundles`, `${libName}.umd.min.js`),
-    scssHelpersInputPathList: [
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_typography.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_cursors.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_color.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_assets.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_breakpoints.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_layout.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_z-index.scss'),
-      // Spacing must be after typography
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_spacing.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_animation.scss'),
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_shadows.scss'),
-      // Card must be after spacing and shadows
-      path.join(rootFolder, 'src/lib', 'src/scss/helpers/_card.scss'),
-    ],
-    scssHelpersOutputPath: path.join(rootFolder, 'src/lib', 'helpers.scss'),
-  },
-};
-
-
-// Add any dependency or peer dependency your library has to `globals` and `external`.
+// This is the list of dependencies and peer dependencies this library has.
 // This is required for UMD bundle users.
 const GLOBALS = {
   // Angular dependencies
@@ -91,17 +64,79 @@ const GLOBALS = {
   'rxjs/BehaviorSubject': 'Rx.BehaviorSubject',
 };
 
-const rollupInputOptions = {
+// General configurations for the build process
+const config = {
+  libName: libName,
+  paths: {
+    rootFolder: rootFolder,
+    distFolder: path.join(rootFolder, 'dist'),
+    srcFolder: path.join(rootFolder, 'src/lib'),
+    tempLibFolder: path.join(rootFolder, 'out-tsc', 'lib'),
+    es2015OutputFolder: path.join(rootFolder, 'out-tsc', 'lib-es2015'),
+    es5Entry: path.join(rootFolder, 'out-tsc', 'lib-es5', `${libName}.js`),
+    libFinalOutputFile: path.join(rootFolder, 'dist', `bundles`, `${libName}.umd.js`),
+    libFinalOutputMinifiedFile: path.join(rootFolder, 'dist', `bundles`, `${libName}.umd.min.js`),
+    scss: {
+      helpersInputs: [
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_typography.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_cursors.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_color.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_assets.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_breakpoints.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_layout.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_z-index.scss'),
+        // Spacing must be after typography
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_spacing.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_animation.scss'),
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_shadows.scss'),
+        // Card must be after spacing and shadows
+        path.join(rootFolder, 'src/lib', 'src/scss/helpers/_card.scss'),
+      ],
+      helpersLibOutput: path.join(rootFolder, 'src/lib', 'helpers.scss'),
+      helpersDistOutput: path.join(rootFolder, 'dist', 'helpers.scss'),
+      globalStylesInput: path.join(rootFolder, 'src/lib', `src/scss/global/terminus-ui.scss`),
+    },
+  },
+  uglify: {
+    compress: true,
+    mangle: true,
+  },
+  sass: {
+    default: {
+      outputStyle: 'compressed',
+      importer: nodeSassImport,
+    },
+    shared: {
+      outputStyle: 'compressed',
+      importer: sassModuleImporter(),
+    },
+    dev: {
+      outputStyle: 'expanded',
+      importer: nodeSassImport,
+    },
+    postCss: [
+      autoprefixer({
+        grid: true,
+      }),
+    ],
+  },
+  delOptions: {
+    force: true,
+  },
+};
+
+// Base shared options for the RollupJS input config
+const rollupInputOptionsBase = {
   // NOTE: Using 'entry' throws a warning: `options.entry is deprecated, use options.input`
   // but when switching to `input` the build is no longer generated (silent failure)
   entry: config.paths.es5Entry,
   plugins: [
-    nodeResolve({
+    rollupNodeResolve({
       es2015: true,
       module: true,
       main: true,
     }),
-    commonjs({
+    rollupCommonjs({
       include: [
         './../node_modules/angular2-ladda/**',
         './../node_modules/ladda/**',
@@ -111,7 +146,7 @@ const rollupInputOptions = {
     }),
   ],
   onwarn: function(warning) {
-    // Suppress this error message... Angular team says to ignore it.
+    // NOTE: Suppress this error message... Angular team says to ignore it.
     // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
     if (warning.code === 'THIS_IS_UNDEFINED') {
       return;
@@ -121,105 +156,104 @@ const rollupInputOptions = {
   external: Object.keys(GLOBALS),
 };
 
-const rollupOutputOptions = {
-  name: camelCase(config.libName),
-  file: config.paths.libFinalOutputFile,
-  sourcemap: true,
-  globals: GLOBALS,
-  format: 'umd',
-};
+// Extend base rollup config with minification
+const rollupMinifiedInputOptions = cloneDeep(rollupInputOptionsBase);
+rollupMinifiedInputOptions.plugins.push(rollupUglify(config.uglify));
 
-const uglifyOptions = {
-  compress: true,
-  mangle: true,
+// General rollup configurations for the build process
+const rollupConfig = {
+  input: rollupInputOptionsBase,
+  inputMinified: rollupMinifiedInputOptions,
+  output: {
+    name: camelCase(config.libName),
+    file: config.paths.libFinalOutputFile,
+    sourcemap: true,
+    globals: GLOBALS,
+    format: 'umd',
+  },
 };
-const sassConfig = {
-  outputStyle: 'compressed',
-  importer: nodeSassImport,
-};
-const postCssPlugins = [
-  autoprefixer({grid: true}),
-];
-const sharedCssSassConfig = {
-  outputStyle: 'compressed',
-  importer: sassModuleImporter(),
-};
-const delOptions = {
-  force: true,
-};
-// Add the uglify plugin to the input options
-const rollupMinifiedInputOptions = Object.assign({}, rollupInputOptions);
-rollupMinifiedInputOptions.plugins.push(rollupUglify(uglifyOptions));
 
 
 
 
 //
-// END OPTIONS / SETUP
+// BEGIN TASKS
 //
 
 
-// Clean all old files
-gulp.task('clean-lib', () => {
+/**
+ * Remove all generated files
+ */
+gulp.task('clean-generated-files', () => {
   return del([
     path.join(config.paths.distFolder, `**/*`),
+    path.join(config.paths.tempLibFolder, `**/*`),
     path.join(config.paths.srcFolder, `**/!(systemjs-angular-loader).js`),
     path.join(config.paths.srcFolder, `**/*.map`),
     path.join(config.paths.srcFolder, `helpers.+(scss|css)`),
-  ], delOptions);
+    path.join(config.paths.srcFolder, `**/*.css`),
+  ], config.delOptions);
 });
 
 
-// Copy library to temp dir
+/**
+ * Copy library to temp working directory for the build process
+ */
 gulp.task('copy-lib', () => {
-  return relativeCopy(`**/*`, config.paths.srcFolder, config.paths.tempLibFolder);
+  return gulp.src(path.join(config.paths.srcFolder, '**/*'))
+    .pipe(gulp.dest(config.paths.tempLibFolder));
 });
 
 
-// Compile SASS
+/**
+ * Compile SCSS to CSS
+ *
+ * 1) Compile to CSS
+ * 2) Strip comments
+ * 3) Write sourcemaps
+ * 4) Run through PostCSS
+ */
 gulp.task('compile-sass', () => {
   return gulp.src(path.join(config.paths.tempLibFolder, `**/*.scss`))
     .pipe(gulpSourcemaps.init())
-    .pipe(sass(sassConfig).on('error', sass.logError))
+    .pipe(gulpSass(config.sass.default).on('error', sass.logError))
+    .pipe(gulpStripComments())
     .pipe(gulpSourcemaps.write())
-    .pipe(postcss(postCssPlugins))
+    .pipe(postcss(config.sass.postCss))
     .pipe(gulp.dest(config.paths.tempLibFolder))
 });
 
 
-// Inline resources
+/**
+ * Inline all component resources (HTML/CSS)
+ */
 gulp.task('inline-resources', () => {
   return inlineResources(config.paths.tempLibFolder);
 });
 
 
-// Compile to ES5
+/**
+ * Compile TypeScript to ES5
+ */
 gulp.task('compile-ngc-es5', () => {
   return ngc({project: `${config.paths.tempLibFolder}/tsconfig.es5.json`});
 });
 
 
-// Compile to ES2015
+/**
+ * Compile TypeScript to ES2015
+ */
 gulp.task('compile-ngc', () => {
   return ngc({project: `${config.paths.tempLibFolder}/tsconfig.json`});
 });
 
 
-// Copy library files
-gulp.task('copy-dts', () => {
-  return relativeCopy('**/*.d.ts', config.paths.es2015OutputFolder, config.paths.distFolder);
-});
-
-// Copy metadata files
-gulp.task('copy-metadata', () => {
-  return relativeCopy('**/*.metadata.json', config.paths.es2015OutputFolder, config.paths.distFolder);
-});
-
-
-// Bundle JS files with Rollup
+/**
+ * Bundle JS files with Rollup (with maps)
+ */
 gulp.task('bundle', () => {
-  return rollup.rollup(rollupInputOptions).then(function(bundle) {
-    return bundle.generate(rollupOutputOptions).then((result) => {
+  return rollup.rollup(rollupConfig.input).then((bundle) => {
+    return bundle.generate(rollupConfig.output).then((result) => {
       // Create bundles dir
       recursiveMkDir(path.join(config.paths.distFolder, `bundles`));
 
@@ -233,10 +267,12 @@ gulp.task('bundle', () => {
 });
 
 
-// Bundle and minify JS files with Rollup
+/**
+ * Bundle & minify JS files with Rollup (with maps)
+ */
 gulp.task('bundleMinified', () => {
-  return rollup.rollup(rollupMinifiedInputOptions).then(function(bundle) {
-    return bundle.generate(rollupOutputOptions).then((result) => {
+  return rollup.rollup(rollupConfig.inputMinified).then((bundle) => {
+    return bundle.generate(rollupConfig.output).then((result) => {
       // Write the output file
       fs.writeFileSync(config.paths.libFinalOutputMinifiedFile, result.code);
 
@@ -247,84 +283,121 @@ gulp.task('bundleMinified', () => {
 });
 
 
-// Copy package files
+/**
+ * Copy package files to dist
+ */
 gulp.task('copy-package-files', () => {
-  return Promise.resolve()
-    .then(() => relativeCopy('LICENSE', config.paths.rootFolder, config.paths.distFolder))
-    .then(() => relativeCopy('package.json', config.paths.rootFolder, config.paths.distFolder))
-    .then(() => relativeCopy('README.md', config.paths.rootFolder, config.paths.distFolder));
-});
-
-
-// Merge SCSS helpers and move to dist
-gulp.task('bundle-scss-helpers', () => {
-  return Promise.resolve()
-    .then(() => mergeFiles(config.paths.scssHelpersInputPathList, config.paths.scssHelpersOutputPath))
-    .then(() => relativeCopy('helpers.scss', config.paths.srcFolder, config.paths.distFolder))
-    .then(() => replace({
-      files: path.join(config.paths.distFolder, 'helpers.scss'),
-      from: [/@import.*/g],
-      to: '',
-    }))
-    .then(() => fs.unlinkSync(path.join(config.paths.srcFolder, 'helpers.scss')));
-});
-
-
-// Generate CSS for shared styles
-gulp.task('generate-css', () => {
-  return gulp.src(path.join(config.paths.srcFolder, `src/scss/global/terminus-ui.scss`))
-    .pipe(sass(sharedCssSassConfig).on('error', sass.logError))
+  return gulp.src([
+    path.join(config.paths.rootFolder, 'LICENSE'),
+    path.join(config.paths.rootFolder, 'package.json'),
+    path.join(config.paths.rootFolder, 'README.md'),
+    path.join(config.paths.es2015OutputFolder, '**/*.metadata.json'),
+    path.join(config.paths.es2015OutputFolder, '**/*.d.ts'),
+  ])
     .pipe(gulp.dest(config.paths.distFolder));
 });
 
 
-// FULL BUILD TASK
-gulp.task('build', gulp.series(
-  'clean-lib',
+/**
+ * Bundle exposed SCSS
+ *
+ * 1) Merge all SCSS helper files
+ * 2) Save to dist
+ */
+gulp.task('bundle-exposed-scss', () => {
+  return gulp.src(config.paths.scss.helpersInputs)
+    .pipe(gulpConcat('helpers.scss'))
+    .pipe(gulp.dest(config.paths.distFolder));
+});
+
+
+/**
+ * Sanitize Exposed SCSS
+ *
+ * 1) Strip comments
+ * 2) Strip @imports
+ * 3) Remove empty lines
+ */
+gulp.task('sanitize-exposed-scss', () => {
+  return gulp.src(path.join(config.paths.distFolder, 'helpers.scss'))
+    .pipe(gulpStripComments())
+    .pipe(gulpReplace(/@import.*/g, ''))
+    .pipe(gulpRemoveEmptyLines())
+    .pipe(gulp.dest(config.paths.distFolder));
+});
+
+
+/**
+ * Generate Exposed CSS
+ *
+ * 1) Compile to CSS
+ * 2) Run through PostCSS/AutoPrefixer
+ */
+gulp.task('generate:css', () => {
+  return gulp.src(config.paths.scss.globalStylesInput)
+    .pipe(gulpSass(config.sass.shared).on('error', sass.logError))
+    .pipe(postcss(config.sass.postCss))
+    .pipe(gulp.dest(config.paths.distFolder));
+});
+
+
+/**
+ * Generate SASS during development
+ *
+ * 1) Compile to CSS
+ * 2) Strip comments
+ * 2) Run through PostCSS/AutoPrefixer
+ */
+gulp.task('compile-sass-dev', () => {
+  return gulp.src(path.join(config.paths.srcFolder, `**/*.scss`))
+    .pipe(gulpSass(config.sass.dev).on('error', sass.logError))
+    .pipe(gulpStripComments())
+    .pipe(postcss(config.sass.postCss))
+    .pipe(gulp.dest(config.paths.srcFolder))
+});
+
+
+
+
+//
+// Complex Tasks
+//
+
+
+// Generate the SCSS helpers file
+gulp.task('generate:exposed-scss', gulp.series(
+  'bundle-exposed-scss',
+  'sanitize-exposed-scss'
+));
+
+
+// Generate a full release build
+gulp.task('generate:build', gulp.series(
+  'clean-generated-files',
   'copy-lib',
   'compile-sass',
   'inline-resources',
   'compile-ngc',
   'compile-ngc-es5',
-  'copy-dts',
-  'copy-metadata',
   'copy-package-files',
   'bundle',
   'bundleMinified',
-  'bundle-scss-helpers',
-  'generate-css'
+  'generate:exposed-scss',
+  'generate:css'
 ));
 
 
 
 
-// HELPERS //
 
-
-
-
-// Copy files maintaining relative paths.
-function relativeCopy(fileGlob, from, to) {
-  return new Promise((resolve, reject) => {
-    glob(fileGlob, { cwd: from, nodir: true }, (err, files) => {
-      if (err) reject(err);
-      files.forEach(file => {
-        const origin = path.join(from, file);
-        const dest = path.join(to, file);
-        const data = fs.readFileSync(origin, 'utf-8');
-        recursiveMkDir(path.dirname(dest));
-        fs.writeFileSync(dest, data);
-        resolve();
-      })
-    })
-  });
-}
-
-// Recursively create a dir.
+/**
+ * Recursively create directories
+ *
+ * @param {String} dir The directory path to create
+ */
 function recursiveMkDir(dir) {
   if (!fs.existsSync(dir)) {
     recursiveMkDir(path.dirname(dir));
     fs.mkdirSync(dir);
   }
 }
-
