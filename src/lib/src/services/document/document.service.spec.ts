8f53ae53ac8347b57d22490f8a8fb5aeb53956ae
Benjamin Charity <ben@benjamincharity.com>
2017-12-13 16:33:33 -0500

    fix(DocumentService): Remove inject to be AoT compliant

--- src/lib/src/services/document/document.service.spec.ts
+++ src/lib/src/services/document/document.service.spec.ts
@@ -6,7 +6,7 @@ import { TsDocumentService } from './document.service';
 describe(`TsDocumentService`, () => {
 
   it(`should return the window object`, () => {
-    const service = new TsDocumentService(DOCUMENT);
+    const service = new TsDocumentService();
 
     expect(service.document).toBeTruthy();
   });
