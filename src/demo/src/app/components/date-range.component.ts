5220df0e96bc56ff24f64bc803ef1bed96097b49
Benjamin Charity <ben@benjamincharity.com>
2017-12-18 09:38:45 -0500

    fix(Datepicker): Now accepting dates via 1 or 2-way binding

--- src/demo/src/app/components/date-range.component.ts
+++ src/demo/src/app/components/date-range.component.ts
@@ -5,14 +5,18 @@ import { Component } from '@angular/core';
   template: `
     <ts-date-range
       [startInitialDate]="myDate"
-      [endInitialDate]="myDate2"
+      endInitialDate="{{ myDate2 }}"
+      startMinDate="{{ minDate }}"
+      startMaxDate="{{ maxDate }}"
       (selectedDate)="printRange($event)"
     ></ts-date-range>
   `,
 })
 export class DateRangeComponent {
-  myDate = new Date(2017, 4, 1);
+  myDate = new Date(2017, 4, 4);
   myDate2 = new Date(2017, 4, 7);
+  minDate = new Date(2017, 4, 2);
+  maxDate = new Date(2017, 4, 20);
 
   printRange(e: any) {
     console.log('Date range!: ', e);
