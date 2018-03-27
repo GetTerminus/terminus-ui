8554bdfedbebdb578076447d42729fa8c352892b
Benjamin Charity <ben@benjamincharity.com>
2018-03-27 09:41:16 -0400

    fix(Services): removed TsWindowService and TsDocumentService
    \`TsWindowService\` and \`TsDocumentService\` are now fully removed from this library. They can be
    imported from \`@terminus/ngx-tools\`.
    
    BREAKING CHANGE:
    \`TsWindowService\` and \`TsDocumentService\` are now fully removed from this library. They can be
    imported from \`@terminus/ngx-tools\`.
    
    ISSUES CLOSED: #693

--- terminus-ui/src/loading-overlay/loading-overlay.module.ts
+++ terminus-ui/src/loading-overlay/loading-overlay.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TsWindowService } from './../services/window/window.service';
 import { TsLoadingOverlayComponent } from './loading-overlay.component';
 import { TsLoadingOverlayDirective } from './loading-overlay.directive';
 export { TsLoadingOverlayDirective } from './loading-overlay.directive';
@@ -10,9 +9,6 @@ export { TsLoadingOverlayDirective } from './loading-overlay.directive';
 @NgModule({
   imports: [
     CommonModule,
-  ],
-  providers: [
-    TsWindowService,
   ],
   declarations: [
     TsLoadingOverlayDirective,
