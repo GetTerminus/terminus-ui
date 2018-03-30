207fa7185e7ee612ab55eb017bfb3a270da375f5
Benjamin Charity <ben@benjamincharity.com>
2018-03-30 10:56:00 -0400

    refactor(Pipes): move type into associated pipe file

--- terminus-ui/src/pipes/date/date.pipe.ts
+++ terminus-ui/src/pipes/date/date.pipe.ts
@@ -8,7 +8,20 @@ import {
   format as formatDate,
 } from 'date-fns';
 
-import { TsDateTypes } from './../../utilities/types/date.types';
+
+/**
+ * Define the allowed date formats for the {@link TsDatePipe}.
+ */
+export type TsDateTypes =
+  // 02/08/2018
+  'short'
+  // February 8th, 2018
+  | 'medium'
+  // Thursday, February 8th, 2018, 12:00:00am
+  | 'extended'
+  // 2018-02-08T05:00:00.000Z
+  | 'timestamp'
+;
 
 
 @Pipe({
