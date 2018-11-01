a6ea48e025302c5f1bf85109f48191976d9f75df
Benjamin Charity <ben@benjamincharity.com>
2018-11-01 14:49:45 -0400

    WIP: deleted old files, upload correct demo dir

--- tooling/ci/s3-deploy.sh
+++ tooling/ci/s3-deploy.sh
@@ -11,10 +11,13 @@ echo 'Current Branch: ' $CIRCLE_BRANCH
 
 releaseBranches=("master" "8.x.x" "9.x.x" "10.x.x")
 
+# TEMP: remove outdated files
+aws s3 rm s3://uilibrary-demo.terminus.ninja --recursive
+
 # Only release docs and demos to S3 if this is a release branch
 if [[ " ${releaseBranches[@]} " =~ " ${CIRCLE_BRANCH} " ]]; then
   aws s3 sync docs s3://uilibrary-docs.terminus.ninja/${CIRCLE_BRANCH}/ --delete
-  aws s3 sync dist s3://uilibrary-demo.terminus.ninja/${CIRCLE_BRANCH}/ --delete
+  aws s3 sync dist/app s3://uilibrary-demo.terminus.ninja/${CIRCLE_BRANCH}/ --delete
 else
   echo "Branch is not a release branch. Skipping documentation and demo release."
   echo "Valid release branches:  ${releaseBranches[*]}"
