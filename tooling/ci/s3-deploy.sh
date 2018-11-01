430eb7079bb9c77588e92e26fe61e697e83230dc
Benjamin Charity <ben@benjamincharity.com>
2018-11-01 12:42:46 -0400

    WIP: try uploading dryrun

--- tooling/ci/s3-deploy.sh
+++ tooling/ci/s3-deploy.sh
@@ -9,11 +9,13 @@
 
 echo 'BRANCH: ' $CIRCLE_BRANCH
 
-# Upload docs
-aws s3 sync docs s3://uilibrary-docs.terminus.ninja/${CIRCLE_BRANCH}/ --delete --dryrun
+releaseBranches=("master" "8.x.x" "9.x.x" "10.x.x")
 
-
-# Upload demos
-# TODO
-# TODO
-# TODO
+# Only release docs and demos to S3 if this is a release branch
+if [[ " ${releaseBranches[@]} " =~ " ${CIRCLE_BRANCH} " ]]; then
+  aws s3 sync docs s3://uilibrary-docs.terminus.ninja/${CIRCLE_BRANCH}/ --delete --dryrun
+  aws s3 sync dist s3://uilibrary-demo.terminus.ninja/${CIRCLE_BRANCH}/ --delete --dryrun
+else
+  echo "Branch is not a release branch. Skipping documentation and demo release."
+  echo "Valid release branches:  ${releaseBranches[*]}"
+fi
