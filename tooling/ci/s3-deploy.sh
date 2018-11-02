bffbb42f255affa78854fa4c2a16a4d08d85e0e8
Benjamin Charity <ben@benjamincharity.com>
2018-11-01 21:21:52 -0400

    WIP: cleaned up

--- tooling/ci/s3-deploy.sh
+++ tooling/ci/s3-deploy.sh
@@ -8,18 +8,14 @@
 . ~/.bashrc
 
 echo 'Current Branch: ' $CIRCLE_BRANCH
-echo 'DIR:'
-cd dist
-ls -d */
-cd ../
 
+# Docs and demos will only deploy when merging to one of these branches
 releaseBranches=("master" "8.x.x" "9.x.x" "10.x.x")
 
 
 # Only release docs and demos to S3 if this is a release branch
 if [[ " ${releaseBranches[@]} " =~ " ${CIRCLE_BRANCH} " ]]; then
   aws s3 sync docs s3://uilibrary-docs.terminus.ninja/${CIRCLE_BRANCH}/ --delete
-  # TODO: Why does 'dist/app' not exist??
   aws s3 sync dist/app s3://uilibrary-demo.terminus.ninja/${CIRCLE_BRANCH}/ --delete
 else
   echo "Branch is not a release branch. Skipping documentation and demo release."
