5a94f27a80e53e538e3317d0478092a23e0167d9
Wendy Yang <atlwendy@gmail.com>
2019-02-27 17:37:52 -0500

    chore(Card): convert card component to integration test

--- terminus-ui/card/src/card.component.spec.ts
+++ terminus-ui/card/src/card.component.spec.ts
@@ -1,116 +1,180 @@
-import { TsCardComponent } from './card.component';
+import { Component, ViewChild } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
+import { createComponent } from '@terminus/ngx-tools/testing';
 
-describe('TsCardComponent', function() {
-  let component: TsCardComponent;
-  let component2: TsCardComponent;
+import { TsCardComponent, TsCardBorderOptions } from './card.component';
+import { TsCardModule } from './card.module';
+import { TsStyleThemeTypes } from '../../utilities/src/public-api';
+
+@Component({
+  template: `
+  <ts-card
+    [isDisabled]="disabled"
+    [flat]="flat"
+    [supportsInteraction]="supportsInteraction"
+    [theme]="theme"
+    [border]="border"
+  >
+  Here is my card!
+  </ts-card>
+  `,
+})
+class TestHostComponent {
+  border: TsCardBorderOptions | undefined;
+  disabled!: boolean;
+  flat!: boolean;
+  supportsInteraction!: boolean;
+  theme: TsStyleThemeTypes | undefined;
+
+  @ViewChild(TsCardComponent)
+  cardComponent!: TsCardComponent;
+}
+
+describe(`TsCardComponent`, function() {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let card: HTMLElement;
+  let cardComponent: TsCardComponent;
+  let cardComponent2: TsCardComponent;
 
   beforeEach(() => {
-    component = new TsCardComponent();
-    component2 = new TsCardComponent();
+    fixture = createComponent(TestHostComponent, [], [TsCardModule]);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    cardComponent = component.cardComponent;
+    card = fixture.debugElement.query(By.css('.c-card')).nativeElement as HTMLElement;
+    cardComponent2 = new TsCardComponent();
   });
 
+  describe(`isDisabled`, function() {
+    test(`should not disable a card`, () => {
+      expect(cardComponent.disabled).toEqual(false);
+      component.disabled = false;
+      fixture.detectChanges();
+      expect(cardComponent.isDisabled).toEqual(false);
+      expect(cardComponent.disabled).toEqual(false);
+      expect(card.classList).not.toContain('c-card--disabled');
+    });
 
-  it(`should exist`, () => {
-    expect(component).toBeTruthy();
-  });
-
-  describe(`centeredContent`, () => {
-    it(`should set and retrieve`, () => {
-      component.centeredContent = true;
-      expect(component.centeredContent).toEqual(true);
+    test(`should disable a card`, () => {
+      component.disabled = true;
+      fixture.detectChanges();
+      expect(cardComponent.isDisabled).toEqual(true);
+      expect(cardComponent.disabled).toEqual(true);
+      expect(card.classList).toContain('c-card--disabled');
     });
   });
 
-  describe(`supportsInteraction`, () => {
-    test(`should set and retrieve`, () => {
+  describe(`isFlat`, function() {
+    test(`should flat a card`, () => {
+      component.flat = true;
+      fixture.detectChanges();
+      expect(cardComponent.flat).toEqual(true);
+      expect(card.classList).toContain('c-card--flat');
+    });
+
+    test(`should not flat a card`, () => {
+      component.flat = false;
+      fixture.detectChanges();
+      expect(cardComponent.flat).toEqual(false);
+      expect(card.classList).not.toContain('c-card--flat');
+    });
+  });
+
+  describe(`supportsInteraction`, function() {
+    test(`should support interaction`, () => {
       component.supportsInteraction = true;
-      expect(component.supportsInteraction).toEqual(true);
+      fixture.detectChanges();
+      expect(cardComponent.supportsInteraction).toEqual(true);
+      expect(card.classList).toContain('c-card--interaction');
+    });
+
+    test(`should not support interaction`, () => {
+      component.supportsInteraction = false;
+      fixture.detectChanges();
+      expect(cardComponent.supportsInteraction).toEqual(false);
+      expect(card.classList).not.toContain('c-card--interaction');
     });
   });
 
+  describe(`centeredContent`, function() {
+    it(`should set position centered`, () => {
+      expect(cardComponent.centeredContent).toEqual(false);
+      cardComponent.centeredContent = true;
+      expect(cardComponent.centeredContent).toEqual(true);
+    });
+  });
 
-  describe(`set aspectRatio()`, () => {
-
+  describe(`set aspectRatio()`, function() {
     it(`should convert the string aspect to a percentage`, () => {
-      component.aspectRatio = '16:9';
-      expect(component.aspectRatioPadding).toEqual('56.25%');
+      cardComponent.aspectRatio = '16:9';
+      fixture.detectChanges();
+      expect(cardComponent.aspectRatioPadding).toEqual('56.25%');
 
-      component.aspectRatio = '4:3';
-      expect(component.aspectRatioPadding).toEqual('75.00%');
+      cardComponent.aspectRatio = '4:3';
+      fixture.detectChanges();
+      expect(cardComponent.aspectRatioPadding).toEqual('75.00%');
     });
-
   });
 
-
-  describe(`id`, () => {
+  describe(`id`, function() {
 
     test(`should set a unique ID and be overwritable and default to UUID if no value is passed in`, () => {
-      expect(component.id).toBeTruthy();
-      expect(component2.id).toBeTruthy();
-      expect(component.id).not.toEqual(component2.id);
+      expect(cardComponent.id).toBeTruthy();
+      expect(cardComponent2.id).toBeTruthy();
+      expect(cardComponent.id).not.toEqual(cardComponent2.id);
 
-      component.id = 'foo';
-      expect(component.id).toEqual('foo');
+      cardComponent.id = 'foo';
+      expect(cardComponent.id).toEqual('foo');
 
-      component.id = null as any;
-      expect(component.id).toEqual(component['_uid']);
+      cardComponent.id = null as any;
+      expect(cardComponent.id).toEqual(cardComponent['_uid']);
     });
 
   });
 
-  describe(`flat`, () => {
-
-    test(`should set flat flag correctly`, () => {
-      component.flat = true;
-      expect(component.flat).toEqual(true);
-    });
-
-  });
-
-  describe(`isDisabled`, () => {
-
-    test(`should set isDisabled flag correctly`, () => {
-      component.isDisabled = true;
-      expect(component.isDisabled).toEqual(true);
-    });
-
-  });
-
-
-  describe(`theme`, () => {
+  describe(`theme`, function() {
 
     test(`should set a default and allow overrides`, () => {
-      expect(component.theme).toEqual('primary');
+      expect(cardComponent.theme).toEqual('primary');
       component.theme = 'warn';
-      expect(component.theme).toEqual('warn');
+      fixture.detectChanges();
+      expect(cardComponent.theme).toEqual('warn');
+      expect(card.classList).toContain('c-card--warn');
     });
 
 
     test(`should do nothing if no value is passed in`, () => {
-      component.theme = '' as any;
-      expect(component.theme).toEqual('primary');
+      cardComponent.theme = '' as any;
+      expect(cardComponent.theme).toEqual('primary');
+      expect(card.classList).toContain('c-card--primary');
     });
 
   });
 
 
-  describe(`border`, () => {
+  describe(`border`, function() {
 
     test(`should do nothing if no value is passed in`, () => {
-      component.border = '' as any;
-      expect(component.border).toEqual('none');
+      cardComponent.border = '' as any;
+      fixture.detectChanges();
+      expect(cardComponent.border).toEqual('none');
+      expect(card.classList).not.toContain('c-card--border');
     });
 
   });
 
 
-  describe(`get borderClass`, () => {
+  describe(`get borderClass`, function() {
 
     test(`should return a string representation of the needed class`, () => {
-      expect(component.borderClass).toEqual('');
+      expect(cardComponent.borderClass).toEqual('');
       component.border = 'top';
-      expect(component.borderClass).toEqual('c-card--border-top');
+      fixture.detectChanges();
+      expect(cardComponent.borderClass).toEqual('c-card--border-top');
+      expect(card.classList).toContain('c-card--border-top');
     });
 
   });
