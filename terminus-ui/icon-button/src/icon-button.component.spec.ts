1ab6e85a855a87c3d8f6549e2159bdf30be27d47
shani-terminus <shani.brown@terminus.com>
2019-03-28 10:14:00 -0400

    chore(IconButton): now with integration tests
    ISSUES CLOSED: #1284

--- terminus-ui/icon-button/src/icon-button.component.spec.ts
+++ terminus-ui/icon-button/src/icon-button.component.spec.ts
@@ -1,32 +1,50 @@
-import { ElementRef } from '@angular/core';
-import { ElementRefMock } from '@terminus/ngx-tools/testing';
+import { Component, ViewChild } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { createComponent } from '@terminus/ngx-tools/testing';
 
 import { TsIconButtonComponent } from './icon-button.component';
+import { TsIconButtonModule } from './icon-button.module';
+
+
+@Component({
+  template: `
+  <ts-icon-button
+    actionName="Menu"
+    buttonType="button"
+    isDisabled="false"
+    (clickEvent)="clickEvent($event)"
+  >delete_forever</ts-icon-button>
+  `,
+})
+class TestHostComponent {
+
+  @ViewChild(TsIconButtonComponent)
+  iconButtonComponent: TsIconButtonComponent;
+}
 
 
 describe(`TsIconButtonComponent`, function() {
-  let elRefMock: ElementRef;
-  let component: TsIconButtonComponent;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let iconButton: HTMLElement;
 
   beforeEach(() => {
-    elRefMock = new ElementRefMock();
-    component = new TsIconButtonComponent(
-      elRefMock,
-    );
+    fixture = createComponent(TestHostComponent, [], [TsIconButtonModule]);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    iconButton = fixture.debugElement.query(By.css('.c-icon-button ')).nativeElement as HTMLElement;
   });
 
 
   test(`should exist`, () => {
     expect(component).toBeTruthy();
+    expect(iconButton).toBeTruthy();
   });
 
-
-  describe(`get hostElement`, () => {
-
-    test(`should return the native element`, () => {
-      expect(component.hostElement).toEqual(elRefMock.nativeElement);
-    });
-
+  test(`should contain icon`, () => {
+    expect(iconButton.children[0].textContent).toContain('delete_forever');
   });
 
 });
