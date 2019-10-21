2b0d6fc26fdef728aaada0171a19da8a06b514a7
Benjamin Charity <ben@benjamincharity.com>
2019-10-21 09:43:47 -0400

    chore(CI): allow whitespace in some instances
    this makes webstorm happy

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,1 +1,4 @@
-module.exports = {extends: '@terminus/stylelint-config-frontend'}
+module.exports = {
+  extends: '@terminus/stylelint-config-frontend',
+  rules: { 'no-eol-whitespace': [true, { ignore: 'empty-lines' }] },
+}
