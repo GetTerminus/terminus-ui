f9b410b1cf96f0e6e2c785a40927315bdceb627f
Benjamin Charity <ben@benjamincharity.com>
2019-11-25 13:03:11 -0500

    fix(CI): use default ci image with orb for aws

---
+++ tooling/ci/replace-docs-emoji.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+// https://github.com/tallesl/node-emojis
+const emojis = require('emojis');
+
+console.log('Fetching "docs/index.html" to emojify...');
+fs.readFile('docs/index.html', 'utf8', (readError, data) => {
+  const emojifiedText = emojis.unicode(data);
+  console.log('Text emojified, saving file...');
+
+  fs.writeFile('docs/index.html', emojifiedText, writeError => {
+    console.log('"docs/index.html" file has been emojified!');
+  });
+});
