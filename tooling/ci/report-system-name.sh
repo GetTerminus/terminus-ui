ee68647c596731492efc66f6e6b1ce362c1fb696
Benjamin Charity <ben@benjamincharity.com>
2019-12-03 11:41:32 -0500

    chore(CI): update manual promotion script

---
+++ tooling/ci/report-system-name.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Report the current system name
+#
+
+. ~/.bashrc
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo "${machine}"
