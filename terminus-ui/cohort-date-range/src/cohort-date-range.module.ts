8790d0a9c2f957a6d310aacbbf47900e88383c6f
Benjamin Charity <ben@benjamincharity.com>
2019-12-11 07:39:43 -0500

    feat(CohortDateRange): no longer blocks user from custom interaction when allowed
    When `allowCustomDates` is true, the user can manually adjust an existing cohort.
    
    ISSUES CLOSED: #1819

--- terminus-ui/cohort-date-range/src/cohort-date-range.module.ts
+++ terminus-ui/cohort-date-range/src/cohort-date-range.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { TsDateRangeModule } from '@terminus/ui/date-range';
 import { TsOptionModule } from '@terminus/ui/option';
 import { TsSelectModule } from '@terminus/ui/select';
+
 import { TsCohortDateRangeComponent } from './cohort-date-range.component';
 
 export * from './cohort-date-range.component';
@@ -17,6 +18,7 @@ export * from './cohort-date-range.component';
     TsDateRangeModule,
     TsSelectModule,
     TsOptionModule,
+    ReactiveFormsModule,
   ],
   exports: [
     TsCohortDateRangeComponent,
