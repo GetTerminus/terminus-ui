50abfbb8a9f0e75d78564b34d2666066f451dc8a
Benjamin Charity <ben@benjamincharity.com>
2020-01-09 12:28:57 -0500

    chore(CI): lint-staged now correctly runs only the staged scss files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -36,7 +36,7 @@ module.exports = {
     const fileList = removeDirectories(files);
 
     return [
-      `yarn run lint:scss ${fileList}`,
+      `yarn run lint:scss:no-files ${fileList}`,
       `git add ${fileList}`,
     ];
   },
