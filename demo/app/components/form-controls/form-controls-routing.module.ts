d560f10c17d0357c712c728648ac92637deb905a
Benjamin Charity <ben@benjamincharity.com>
2020-01-15 11:48:28 -0500

    chore(Demo): add form control alignment demo

---
+++ demo/app/components/form-controls/form-controls-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FormControlsComponent } from './form-controls.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FormControlsComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class FormControlsRoutingModule { }
---
+++ demo/app/components/form-controls/form-controls.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TsButtonModule } from '@terminus/ui/button';
+import { TsCardModule } from '@terminus/ui/card';
+import { TsCheckboxModule } from '@terminus/ui/checkbox';
+import { TsInputModule } from '@terminus/ui/input';
+import { TsOptionModule } from '@terminus/ui/option';
+import { TsRadioGroupModule } from '@terminus/ui/radio-group';
+import { TsSelectModule } from '@terminus/ui/select';
+import { TsSelectionListModule } from '@terminus/ui/selection-list';
+import { TsSpacingModule } from '@terminus/ui/spacing';
+import { TsToggleModule } from '@terminus/ui/toggle';
+
+import { FormControlsRoutingModule } from './form-controls-routing.module';
+import { FormControlsComponent } from './form-controls.component';
+import { RowComponent } from './row.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormControlsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TsButtonModule,
+    TsCardModule,
+    TsCheckboxModule,
+    TsInputModule,
+    TsOptionModule,
+    TsRadioGroupModule,
+    TsSelectionListModule,
+    TsSelectModule,
+    TsSpacingModule,
+    TsToggleModule,
+  ],
+  declarations: [
+    FormControlsComponent,
+    RowComponent,
+  ],
+})
+export class FormControlsModule {}
