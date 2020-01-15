c9d8980a6cd088962e2a3f675e4bd4d0b59dfa0c
Benjamin Charity <ben@benjamincharity.com>
2020-01-15 11:37:52 -0500

    fix(SelectionList): now has consistent vertical height with or without values

--- terminus-ui/selection-list/src/selection-list.component.ts
+++ terminus-ui/selection-list/src/selection-list.component.ts
@@ -118,6 +118,7 @@ export type TsSelectionListComparator = (a: unknown, b: unknown) => boolean;
     '[class.ts-selection-list--required]': 'isRequired',
     '[class.ts-selection-list--disabled]': 'isDisabled',
     '[class.ts-selection-list--single]': '!allowMultiple',
+    '[class.ts-selection-list--multiple]': 'allowMultiple',
     '[attr.aria-owns]': 'panelOpen ? optionIds : null',
     '[attr.aria-required]': 'isRequired.toString()',
     '[attr.aria-multiselectable]': 'allowMultiple',
