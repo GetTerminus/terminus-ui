48e44e3a3a371c38eebcc0cdb45354f75e1c2d8f
Benjamin Charity <ben@benjamincharity.com>
2020-01-21 08:29:40 -0500

    fix(SelectionList): single and multiple select now have matching height

--- demo/app/components/form-controls/row.component.ts
+++ demo/app/components/form-controls/row.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'row',
-  templateUrl: 'row.component.html',
+  templateUrl: './row.component.html',
 })
 
 export class RowComponent implements OnInit {
