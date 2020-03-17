b581f5d3b3623568d8d2f314b742d8edc453e2b2
atlwendy <atlwendy@gmail.com>
2020-03-17 13:54:40 -0400

    rearrange flows

--- tooling/ci/check-demo-up-and-running.sh
+++ tooling/ci/check-demo-up-and-running.sh
@@ -8,13 +8,11 @@
 
 . ~/.bashrc
 
-# Build the app with the correct base href
-#export CYPRESS_CACHE_FOLDER=../../cache/Cypress
-#export CYPRESS_CACHE_FOLDER=cache/Cypress
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install --save-dev start-server-and-test
 npm install ngx-perfect-scrollbar
 npm install text-mask-addons
 npm install text-mask-core
+sudo npm link gulp
 
 
