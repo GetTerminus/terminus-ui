e5125f9953d5d60dd35c5004c3cdc6406af9295c
atlwendy <atlwendy@gmail.com>
2020-03-18 10:32:38 -0400

    clean up

--- tooling/ci/check-demo-up-and-running.sh
+++ tooling/ci/check-demo-up-and-running.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 #
-# Check the demo site is up and running
 #
-# Use cypress to check the demo site is up and running
+# Install all the necessary packages for spinning up a server
 #
 
 . ~/.bashrc
 
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install --save-dev start-server-and-test
-#npm install ngx-perfect-scrollbar
-#npm install text-mask-addons
-#npm install text-mask-core
+npm install ngx-perfect-scrollbar
+npm install text-mask-addons
+npm install text-mask-core
 sudo npm link gulp
 
 
