454226d1ee520a889b6f19001905254e54d9b29e
atlwendy <atlwendy@gmail.com>
2020-03-18 08:55:51 -0400

    optimizing installation

--- tooling/ci/check-demo-up-and-running.sh
+++ tooling/ci/check-demo-up-and-running.sh
@@ -10,9 +10,9 @@
 
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install --save-dev start-server-and-test
-npm install ngx-perfect-scrollbar
-npm install text-mask-addons
-npm install text-mask-core
+#npm install ngx-perfect-scrollbar
+#npm install text-mask-addons
+#npm install text-mask-core
 sudo npm link gulp
 
 
