f537f89bc68f9a914a7ca7b9fb3f7cec5d8bfd64
Benjamin Charity <ben@benjamincharity.com>
2020-04-10 09:31:47 -0400

    fix(CI): final tweaks for Ivy

--- ngcc.config.js
+++ ngcc.config.js
@@ -3,11 +3,6 @@ module.exports = {
   packages: {
     '@terminus/ui': {
       entryPoints: { './dist/library': { ignore: true } },
-      ignorableDeepImportMatchers: [
-        /text-mask-core\//,
-        /text-mask-addons\//,
-        /popper.js\//,
-      ],
     },
   },
 };
