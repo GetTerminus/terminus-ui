abb7c7421b8637af4491fc57f8692abd073e6be2
Benjamin Charity <ben@benjamincharity.com>
2020-04-14 11:40:44 -0400

    fix(CI): remove home prefix for demos path

--- tooling/ci/demos-predeploy.sh
+++ tooling/ci/demos-predeploy.sh
@@ -17,7 +17,7 @@ find . -regextype posix-extended -regex '.*\.(html|css|js)' -type f -delete
 cd ../
 
 # Move new files into the cloned repo
-mv -f -v "$HOME"/dist/demo/* "$HOME"/ui-demos-release/
+mv -f -v dist/demo/* ui-demos-release/
 
 # Clone the index.html file as 404.html to support deep linking
 cp "$HOME"/ui-demos-release/index.html "$HOME"/ui-demos-release/404.html
