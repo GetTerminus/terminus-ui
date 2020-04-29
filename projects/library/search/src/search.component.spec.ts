c67314e720ee521ae70de1d63ad6eedab12c97ba
atlwendy <atlwendy@gmail.com>
2020-04-29 16:05:47 -0400

    chore(Search): convert out of dated specs to integration tests

--- projects/library/search/src/search.component.spec.ts
+++ projects/library/search/src/search.component.spec.ts
@@ -1,15 +1,77 @@
 import {
-  FormBuilder,
-  FormGroup,
-} from '@angular/forms';
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+import { FormGroup } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { createComponent } from '@terminus/ngx-tools/testing';
+import { TsButtonComponent } from '@terminus/ui/button';
+import { TsInputComponent } from '@terminus/ui/input';
+import {
+  getInputElement,
+  sendInput,
+} from '@terminus/ui/input/testing';
 
 import { TsSearchComponent } from './search.component';
+import { TsSearchModule } from './search.module';
+
+@Component({
+  template: `
+      <ts-search
+        [isDisabled]="isDisabled"
+        [isSubmitting]="inProgress"
+        [isFocused]="isFocused"
+        [initialValue]="startingValue"
+        [autoSubmit]="shouldAutoSubmit"
+        [userCanClear]="userCanClear"
+        (submitted)="onSubmit($event)"
+        (cleared)="onClear()"
+        (changed)="onChange($event)"
+      ></ts-search>
+  `,
+})
+class TestHostComponent implements OnInit {
+  isDisabled = false;
+  isFocused = true;
+  inProgress = false;
+  startingValue = '';
+  shouldAutoSubmit = true;
+  noValidationOrHint = false;
+  userCanClear = true;
+
+  @ViewChild(TsButtonComponent, { static: true })
+  public buttonComponent!: TsButtonComponent;
+
+  @ViewChild(TsInputComponent)
+  public inputComponent!: TsInputComponent;
+
+  @ViewChild(TsSearchComponent)
+  public searchComponent!: TsSearchComponent;
+
+  onSubmit(query: string): void { }
+
+  onClear(): void { }
+
+  onChange(value: string): void {
+    console.log('DEMO: search input changed: ', value);
+  }
+  ngOnInit(): void { }
+}
 
 describe('TsSearchComponent', function() {
-  let component: TsSearchComponent;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let search: HTMLElement;
+  let searchComponent;
 
   beforeEach(() => {
-    component = new TsSearchComponent(new FormBuilder());
+    fixture = createComponent(TestHostComponent, [], [TsSearchModule]);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    search = fixture.debugElement.query(By.css('.ts-search')).nativeElement;
+    searchComponent = component.searchComponent;
   });
 
   test(`should exist`, () => {
@@ -19,7 +81,8 @@ describe('TsSearchComponent', function() {
   describe(`isDisabled`, () => {
     test(`should set and retrieve`, () => {
       component.isDisabled = true;
-      expect(component.isDisabled).toEqual(true);
+      const inputElement = getInputElement(fixture);
+      expect(inputElement.disabled).toEqual(false);
     });
   });
 
@@ -32,126 +95,135 @@ describe('TsSearchComponent', function() {
 
   describe(`isSubmitting`, () => {
     test(`should set and retrieve`, () => {
-      component.isSubmitting = true;
-      expect(component.isSubmitting).toEqual(true);
+      component.shouldAutoSubmit = false;
+      component.inProgress = true;
+      fixture.detectChanges();
+      const button = fixture.debugElement.queryAll(By.css('.c-button'))[0].nativeElement as HTMLButtonElement;
+      expect(button.getAttribute('disabled')).toEqual('true');
     });
   });
 
   describe(`userCanClear`, () => {
     test(`should set and retrieve`, () => {
       component.userCanClear = true;
-      expect(component.userCanClear).toEqual(true);
+      fixture.detectChanges();
+      sendInput(fixture, 'foo');
+      fixture.detectChanges();
+      const clearButton = fixture.debugElement.query(By.css('.c-input__clear')).nativeElement;
+      expect(clearButton).toBeTruthy();
     });
   });
 
   describe(`currentQuery`, () => {
     test(`should return the current query with no trailing or leading whitespace`, () => {
-      component.initialValue = 'foo';
-      component.ngOnInit();
-      expect(component.currentQuery).toEqual('foo');
+      searchComponent.initialValue = 'foo';
+      searchComponent.ngOnInit();
+      expect(searchComponent.currentQuery).toEqual('foo');
 
-      component.searchForm.patchValue({ query: ' foo 23 ' });
-      expect(component.currentQuery).toEqual('foo 23');
+      searchComponent.searchForm.patchValue({ query: ' foo 23 ' });
+      expect(searchComponent.currentQuery).toEqual('foo 23');
 
-      component.searchForm.patchValue({ query: null });
-      expect(component.currentQuery).toEqual('');
+      searchComponent.searchForm.patchValue({ query: null });
+      expect(searchComponent.currentQuery).toEqual('');
     });
 
     test(`should return empty string if current query length below required minimum`, () => {
-      component.initialValue = 'a';
-      component.ngOnInit();
-      expect(component.currentQuery).toEqual('');
+      searchComponent.initialValue = 'a';
+      searchComponent.ngOnInit();
+      expect(searchComponent.currentQuery).toEqual('');
     });
   });
 
   describe(`ngOnInit()`, () => {
     test(`should seed the query with an initial value if one exists`, () => {
       const STRING = 'foo';
-      component.initialValue = STRING;
-      expect(component.query).toEqual('');
+      searchComponent.initialValue = STRING;
+      expect(searchComponent.query).toEqual('');
 
-      component.ngOnInit();
+      searchComponent.ngOnInit();
 
-      expect(component.searchForm.get('query')!.value).toEqual(STRING);
+      expect(searchComponent.searchForm.get('query')!.value).toEqual(STRING);
     });
   });
 
   describe(`keyup()`, () => {
     beforeEach(() => {
-      component.changed.emit = jest.fn();
-      component.debouncedEmit = jest.fn();
+      searchComponent.changed.emit = jest.fn();
+      searchComponent.debouncedEmit = jest.fn();
     });
 
     test(`should emit each change`, () => {
-      component.initialValue = 'foo';
-      component.ngOnInit();
-      component.keyup();
+      searchComponent.initialValue = 'foo';
+      searchComponent.autoSubmit = false;
+      fixture.detectChanges();
+      searchComponent.ngOnInit();
+      searchComponent.keyup();
 
-      expect(component.changed.emit).toHaveBeenCalledWith('foo');
-      expect(component.debouncedEmit).not.toHaveBeenCalled();
+      expect(searchComponent.changed.emit).toHaveBeenCalledWith('foo');
+      expect(searchComponent.debouncedEmit).not.toHaveBeenCalled();
     });
 
     describe(`with auto submit enabled`, () => {
       test(`should call the debounced emit if the form is valid`, () => {
-        component.autoSubmit = true;
-        component.initialValue = 'foo';
-        component.ngOnInit();
-        component.keyup();
+        searchComponent.autoSubmit = true;
+        searchComponent.initialValue = 'foo';
+        searchComponent.ngOnInit();
+        searchComponent.keyup();
 
-        expect(component.debouncedEmit).toHaveBeenCalled();
+        expect(searchComponent.debouncedEmit).toHaveBeenCalled();
       });
 
       test(`should NOT call the debounced emit if the form is NOT valid`, () => {
-        component.autoSubmit = true;
-        component.initialValue = 'foo&';
-        component.ngOnInit();
-        component.keyup();
+        searchComponent.autoSubmit = true;
+        searchComponent.initialValue = 'foo&';
+        searchComponent.ngOnInit();
+        searchComponent.keyup();
 
-        expect(component.debouncedEmit).not.toHaveBeenCalled();
+        expect(searchComponent.debouncedEmit).not.toHaveBeenCalled();
       });
     });
 
     describe(`without auto submit enabled`, () => {
       test(`should NOT call the debounced emit even if the form is valid`, () => {
-        component.autoSubmit = false;
-        component.initialValue = 'foo';
-        component.ngOnInit();
-        component.keyup();
+        searchComponent.autoSubmit = false;
+        searchComponent.initialValue = 'foo';
+        searchComponent.ngOnInit();
+        searchComponent.keyup();
 
-        expect(component.debouncedEmit).not.toHaveBeenCalled();
+        expect(searchComponent.debouncedEmit).not.toHaveBeenCalled();
       });
     });
   });
 
   describe(`emitSubmit()`, () => {
     beforeEach(() => {
-      component.submitted.emit = jest.fn();
-      component.initialValue = 'foo';
-      component.ngOnInit();
+      searchComponent.submitted.emit = jest.fn();
+      searchComponent.initialValue = 'foo';
+      searchComponent.ngOnInit();
     });
 
     test(`should emit an event if the form is valid`, () => {
-      component['emitSubmit']();
-      expect(component.submitted.emit).toHaveBeenCalledWith({ query: 'foo' });
+      searchComponent['emitSubmit']();
+      expect(searchComponent.submitted.emit).toHaveBeenCalledWith({ query: 'foo' });
     });
 
     test(`should call emitSubmit via debouncedEmit`, () => {
       jest.useFakeTimers();
-      component.debouncedEmit();
+      searchComponent.debouncedEmit();
       jest.runAllTimers();
 
-      expect(component.submitted.emit).toHaveBeenCalledWith({ query: 'foo' });
+      expect(searchComponent.submitted.emit).toHaveBeenCalledWith({ query: 'foo' });
     });
   });
 
   describe(`get searchFormControl`, function() {
     test(`should return the control`, function() {
-      expect(component.searchFormControl!.statusChanges).toBeTruthy();
+      expect(searchComponent.searchFormControl!.statusChanges).toBeTruthy();
     });
 
     test(`should return null if the control doesn't exist`, function() {
-      component.searchForm = new FormGroup({});
-      expect(component.searchFormControl).toEqual(null);
+      searchComponent.searchForm = new FormGroup({});
+      expect(searchComponent.searchFormControl).toEqual(null);
     });
   });
 });
