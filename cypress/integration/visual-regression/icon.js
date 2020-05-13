9012c6b76f1e1a2cef32454beb9d1adfb38ce842
Benjamin Charity <ben@benjamincharity.com>
2020-05-13 11:09:36 -0400

    Revert "fix(Icon): icon should have a fixed height"
    This reverts commit 6e690c2912393117107ffc6c882d41c643b437d9.
    
    This was causing several other alignment issues within buttons. Rolling
    back until a better solution is found.

--- cypress/integration/visual-regression/icon.js
+++
@@ -1,6 +0,0 @@
-describe(`Icon Component`, () => {
-  it(`should load the icon page`, () => {
-    cy.visit('http://localhost:4200/components/button');
-    cy.percySnapshot('Icon: all icons');
-  });
-});
--- projects/visual-regression/src/app/components/icon/icon.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-icon',
-  templateUrl: './icon.component.html',
-})
-export class IconComponent {
-}
